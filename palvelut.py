45a04b27c4ae8f7ee828bbcb1a0ac2cd26ab2635
anssimv <anssi.vilppunen@gmail.com>
2022-03-12 21:14:37 +0200

    na

--- palvelut.py
+++ palvelut.py
@@ -1,8 +1,58 @@
+
+import random
+
+#       -=__        #=_   +=none 
+
+
+
 class Asiakas:
-    pass
+
+
+    #luo_nro
+    def __init__(self):
+        self.__nimi = 
+        self.__asiakasnro = [ensimmainen_osa, "_" toinen_osa, "-" kolmas_osa]
+        self.__ika
+        
+        
+        
+        
+        
+    ensimmainen_osa = random.randint(1, 999)
+    toinen_osa = random.randint(1, 999)
+    kolmas_osa = random.randint(1, 999)
+    
+        pass
+    
+    def __get_nimi():
+        print(nimi)
+    
+        pass
+    
+    def __get_ika():
+        print(ika)
+    
+    
+    
+        pass
+
+
+
+
+
+
+
+
+
 
 class Palvelu:
-    pass
+    def __init__(tuotenimi, asiakkaat):
+        pass
+        
+            def luo_asakasrivi(Asiakas):
+    #palvelu
+    #tuotenimi
+                pass
 
-class ParemiPalvelu:
+class ParemiPalvelu(Palvelu):
     pass
