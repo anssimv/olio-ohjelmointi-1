958fd0a36b0a033a77a665eb4fc2c7781dd52aa2
anssimv <anssi.vilppunen@gmail.com>
2022-03-14 08:10:23 +0200

    na

---
+++ palvelut.py
@@ -0,0 +1,153 @@
+
+import random
+
+   #   -=__        #=_   +=none 
+
+
+
+class Asiakas:
+    """
+    :ivar asiakkaan nimi: asiakkaan nimi
+    :type: str
+    :ivar asiakasnumero: koostuu numeroista, arvotaan random.randitilla
+    :type: int[]
+    :ivar: asiakkaan ikä: asiakkaan ikä
+    :type: int
+    """
+
+    #luo_nro
+    def __init__(self, nimi, ika):
+        self.__nimi = ""
+        self.__asiakasnro = (f"{ensimmainen_osa}, _ {toinen_osa}, - {kolmas_osa}")
+        self.__ika = ""
+ensimmainen_osa = random.randint(1, 99)
+                
+toinen_osa = random.randint(1, 999)
+            
+kolmas_osa = random.randint(1, 999)
+        
+        
+        
+        
+    
+    def get_tiedot(asiakas):
+        
+        raise ValueError("Annettiin negatiivinen luku.")
+        
+        print(nimi)
+    
+    
+        pass
+    
+    
+
+    def __get_asiakasnro():
+    
+    
+    
+        pass
+
+
+
+
+
+
+
+
+
+
+    class Palvelu:
+        """
+        :ivar: Tuotenimi: minkä tuotteen asiakkaat listataan
+        :type: str
+        :ivar: asiakkaat: kertoo ketkä tuotetta käyttävät 
+        :type: Asiakas[]
+        """
+        def __init__(self, tuotenimi, asiakkaat):
+
+
+            
+            
+            
+            
+        def tulosta_asiakkaat(Palvelu):
+            print(Palvelu.asiakkaat)
+
+            
+            
+            
+            
+            def get_luo_asiakasrivi(Asiakas):
+                print(f"{nimi} ({asiakas_nro}) on {ika}-vuotias")
+
+            """
+            :ivar: luo_asiakasrivi(Asiakas): perii luokasta Asiakas nimen, asiakasnumeron
+            ja iän ja luo niistä tietorivin.
+            :type: str
+            :ivar: lisaa_asiakas
+            
+
+            :param: erotin: numeron osien väliin alitettava väliviiva
+            :type: str
+            :ivar: poista_asiakas(Asiakas): poistaa asiakkaan tiedot
+            :type: [str]
+            :ivar: tulosta_asiakkaat: tulostaa kaikkien asiakkaiden tiedot
+            :type: str
+            """
+            
+
+        def poista_asiakas
+            poistettava = (input("kenet poistetaan"))
+            try: poistettava in edut:
+                edut.pop(poistettava)
+
+            except ValueError: ("asiakasta ei ole listassa")
+
+
+        
+
+
+        
+            #def luo_asakasrivi(Asiakas):
+    #palvelu
+    #tuotenimi
+                #pass
+
+
+
+
+
+
+    class ParemiPalvelu(Palvelu):
+#
+
+        def __init__(self, edut):
+            edut = []
+
+        def lisaa_etu():
+            edut.append(etu)
+
+            """
+            :ivar: lisaa_etu: lisää edun listaan
+            :type: str
+            """
+
+        def poista_etu():
+            poistettava = (input("Anna kokonaisluku: "))
+            try: poistettava in edut:
+                edut.pop(poistettava)
+
+            except ValueError: ("Etua ei ole listassa")
+
+            
+            """
+            :ivar: poista_etu: poistaa edun listasta
+            :type: str
+            """
+        def tulosta_edut():
+            print(edut)
+            """
+            :IVAR: tulosta_edut: tulostaa edut
+            :type: str
+
+            """
